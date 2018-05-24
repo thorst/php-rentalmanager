e8c06f0fcd027b2cb3aabbc2d31553f319d358a0
toddm <toddmhorst@yahoo.com>
2018-05-23 21:31:04 -0400

    removed bootstrap locally

--- login.php
+++ login.php
@@ -10,7 +10,7 @@
     <title>Signin Template for Bootstrap</title>
 
     <!-- Bootstrap core CSS -->
-    <link href="scripts/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
     <!-- Custom styles for this template -->
     <link href="scripts/signin.css" rel="stylesheet">
